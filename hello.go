e1336c32018e931648239a623beaee9c4dfef611
Yanis <yanis.urbis@gmail.com>
2020-03-24 10:39:24 +0300

    Add Item struct

--- hello.go
+++ hello.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Item struct {
+	Value interface{}
+	Next *Item
+	Prev *Item
+}
+
+//type List interface {
+//	Len() int
+//}
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(Item{
+		Value: "Hello",
+		Next:  &Item{
+			Value: "World",
+			Next:  nil,
+			Prev:  nil,
+		},
+		Prev:  nil,
+	})
 }
