e15e09cd204f88761536a7e8836ae577ab8c7330
Yanis <yanis.urbis@gmail.com>
2020-03-28 16:08:52 +0300

    Copy tests from the main repo

--- cache_test.go
+++ cache_test.go
@@ -160,4 +160,43 @@ func TestCache(t *testing.T) {
 		_, found4 := cache.Get("3")
 		require.False(t, found4)
 	})
+
+	t.Run("empty cache", func(t *testing.T) {
+		c := NewCache(10)
+
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		c := NewCache(5)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
 }
